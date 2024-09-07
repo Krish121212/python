a4dbb89af6f14b0c3f32bb192b867513c59fb3b0
surya pelluru <maladikrishnakishore@gmail.com>
2024-09-07 13:25:37 +0530

    python

--- 01-Variables/01-variable.py
+++ 01-Variables/01-variable.py
@@ -1,4 +1,4 @@
-#string 
+# Data Types
 str1 = "Krishna"
 str2 = "Kishore"
 result = str1 + " " + str2
@@ -11,5 +11,16 @@ print(a,b)
 # Python is case sensitive in booleon True and true are different.
 c= True
 print(c)
+
+# String
+a= 'This is Krishna'
+b= "This is also Krishna"
+c= """
+This is multi line string
+"""
+# when to use which string. d fails, e runs
+d= 'I'm krishna'
+e= "i'm Krishna"
+
 
 
