b77bf0ae22bb0fce2be7e1c40e228733615f3aa1
surya pelluru <maladikrishnakishore@gmail.com>
2024-09-07 20:56:54 +0530

    python

--- 01-Variables/02-Operators.py
+++ 01-Variables/02-Operators.py
@@ -6,3 +6,13 @@ word1= a<b
 word2= a==b
 word3= a!=b
 print(word,word1,word2,word3)
+
+# Logical operators
+# AND, NOT, OR
+a=False
+b=True
+word = a and b
+word1= a or b
+word2= not b
+word3= not a
+print(word,word1,word2,word3)
