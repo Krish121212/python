b2ba0fb5157b40658246360efe035bde8331e384
surya pelluru <maladikrishnakishore@gmail.com>
2024-09-08 18:26:44 +0530

    python

--- 01-Variables/03-strings.py
+++ 01-Variables/03-strings.py
@@ -7,10 +7,10 @@ print(sample_string[9])
 # slicing
 print(sample_string[11:17])
 print(sample_string[:]) # you can get complete string i.e "This is my sample string"
-print(sample_string[5:])
-print(sample_string[:5])
-print(sample_string[::2])
-print(sample_string[::-1])
+print(sample_string[5:]) # print started from 5th character as we specified starting point as 5
+print(sample_string[:5]) # starting five letters will come as we specified end point as 5
+print(sample_string[::2]) # starting,ending ae not specified - but step size is 2. so every two alternate alphabet will be printed
+print(sample_string[::-1]) # reverse of string
 
 
 
