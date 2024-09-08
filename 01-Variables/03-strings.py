be96af421309457c9cb55edfb72eda140456d633
surya pelluru <maladikrishnakishore@gmail.com>
2024-09-08 18:00:20 +0530

    python

--- 01-Variables/03-strings.py
+++ 01-Variables/03-strings.py
@@ -7,7 +7,7 @@ print(sample_string[9])
 # slicing
 print(sample_string[11:17])
 print(sample_string[:]) # you can get complete string i.e "This is my sample string"
-print(sample_string[1:])
+print(sample_string[4:])
 print(sample_string[:5])
 print(sample_string[::2])
 print(sample_string[::-1])
