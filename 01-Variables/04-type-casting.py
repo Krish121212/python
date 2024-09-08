beff21e88fbce73655d55ad9784855ff135018a3
surya pelluru <maladikrishnakishore@gmail.com>
2024-09-09 01:01:15 +0530

    python

---
+++ 01-Variables/04-type-casting.py
@@ -0,0 +1,11 @@
+# string to list conversion
+sample_str = "This is Python and Im krishna"
+sample_str_list = list(sample_str)
+print(sample_str_list)
+
+# string to tuple conversion
+sample_str = "This is Python and Im krishna"
+sample_str_list = tuple(sample_str)
+print(sample_str_list)
+
+
