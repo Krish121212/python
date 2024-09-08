ca57140c0f3c42c7730682efe93e597af855f7b7
surya pelluru <maladikrishnakishore@gmail.com>
2024-09-09 01:06:58 +0530

    python

--- 01-Variables/04-type-casting.py
+++ 01-Variables/04-type-casting.py
@@ -10,9 +10,7 @@ print(sample_str_list)
 
 # Accepting input from a user
 user_input= input("Enter a number:-")
-print(user_input, type(user_input))
+print(user_input, type(user_input)) # By default input will be of string type
 
-add_88 = int(user_input) + 88
+add_88 = int(user_input) + 88 # Hence we need to change the type to int - if needed
 print(add_88)
-
-
