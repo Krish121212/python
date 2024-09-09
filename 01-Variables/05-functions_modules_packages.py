51eab71c3cee05829474afaf018e6568907e3653
surya pelluru <maladikrishnakishore@gmail.com>
2024-09-09 11:42:47 +0530

    python

---
+++ 01-Variables/05-functions_modules_packages.py
@@ -0,0 +1,16 @@
+# calculator function approach
+num1 = 100
+num2 = 20
+
+def addition():
+    addition= num1 + num2
+    print(addition)
+
+def sub():
+    sub= num1 + num2
+    print(sub)
+
+def mul():
+    mul= num1 + num2
+    print(mul)
+
